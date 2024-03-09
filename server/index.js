d71c6058a180af3bb723e6f025d4d9c9f1261fa3
nuclyd <nuclealydie@gmail.com>
2024-03-09 14:00:41 +0100

    add API fetching test route for client

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
 	res.send('Server is successfully connected!');
 })
 
+// Test API fetching by frontend with random number:
+app.get('/api/test', (req, res) => {
+	let randomNumber = Math.floor(Math.random() * 100);
+	console.log('random number generated is:', randomNumber);
+	res.send(randomNumber.toString());
+})
+
 // GET 'protests.html'-actual/past protests list page from dir 'test-views':
 app.get("/protests", (req, res) => {
         const filePath = path.join(".", "test-views", "protests.html")

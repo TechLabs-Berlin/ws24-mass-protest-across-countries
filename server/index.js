44cccd4f93b99ba7f24f95b11aedcb91bf7dba81
nuclyd <nuclealydie@gmail.com>
2024-03-09 14:05:22 +0100

    add middleware to solve CORS issue

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// Middleware to solve CORS issue:
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  })
+
 // Test server successful connecton:
 app.get('/', (req, res) => {
 	console.log('Test connection');

88018fec4fee7c4f82d12c051efc035b9601ddd5
nuclyd <nuclealydie@gmail.com>
2024-03-10 17:40:36 +0100

    add Protests route in client App

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import ApiTestPage from "./components/ApiTestPage";
+import ProtestsPage from "./components/ProtestsPage";
 
-// There are various different kinds of routers in the React Router Dom package.
+// There are various different kinds of routers in the React Router Dom package. 
 // Within the router, routes can be set up by declaring individual routes with a URL path and the component to be
 // rendered upon accessing said path.
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/api-test" element={<ApiTestPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<LandingPage />} />
+					<Route path="/api-test" element={<ApiTestPage />} />
+					<Route path="/protests" element={<ProtestsPage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;

0e7eb26be0d5ad9e6f6bfedfca61c59c99cc01ca
nuclyd <nuclealydie@gmail.com>
2024-03-13 18:40:21 +0100

    install mongoose and connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
+const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
 const ExpressError = require("./utils/ExpressError");
+
+// Connect to MongoDB:
+mongoose.connect("mongodb://localhost:27017/fm-protest");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 // Middleware to solve CORS issue:
 app.use(function(req, res, next) {

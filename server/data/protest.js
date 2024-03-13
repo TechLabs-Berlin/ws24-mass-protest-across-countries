8688d06e4bf15a7b4d1f8048c1cbf44566cfde45
nuclyd <nuclealydie@gmail.com>
2024-03-13 18:49:06 +0100

    add protest schema and model

---
+++ server/data/protest.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/fm-protest");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+// Define schema
+const protestSchema = new Schema({
+    event_date: Date,
+    year: Number,
+    event_type: String,
+    mood: String,
+    district: String,
+    source: String,
+    categories: String,
+    description: String,
+    crowd_size: String,
+    longitude: Number,
+    latitude: Number,
+  });
+  
+// Create model and export:
+module.exports = mongoose.model('Protest', protestSchema);

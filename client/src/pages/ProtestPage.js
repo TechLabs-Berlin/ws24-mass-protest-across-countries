9cd7c853b40e35e31ebfd04ec1e6c1c59573bf70
nuclyd <nuclealydie@gmail.com>
2024-03-15 17:16:45 +0100

    refactor code and rename ProtestPage.js

---
+++ client/src/pages/ProtestPage.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DropDowns from './Protest/DropDowns';
+import ProtestList from './Protest/ProtestList';
+import YearSelector from './Protest/YearSelector';
+
+const DEV_URL = "http://localhost:8000/";
+const list_year = [2023, 2024];
+const list_categories = [""];
+const district = [""];
+
+export default function ProtestPage() {
+    const [data, setData] = useState([]);
+    const [selectedYear, setSelectedYear] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedDistrict, setSelectedDistrict] = useState("");
+
+    const getProtestList = async () => {
+        try {
+            const queryParams = [
+                ...(selectedYear ? [`year=${selectedYear}`] : []),
+                ...(selectedCategory ? [`categories=${selectedCategory}`] : []),
+                ...(selectedDistrict ? [`district=${selectedDistrict}`] : [])
+            ];
+            
+            const url_param = queryParams.length > 0 ? '?' + queryParams.join('&') : '';
+
+            const response = await axios.get(DEV_URL + "api/protest" + url_param);
+            const protestData = response.data.slice(0, 10);
+            setData(protestData);
+            console.log(protestData);
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getProtestList();
+    }, [selectedYear, selectedCategory, selectedDistrict]); // Add selected values as dependencies to trigger the effect when they change
+    
+    const handleYearSelect = async (year) => {
+        setSelectedYear(year);
+    };
+
+    return (
+        <div>
+            <DropDowns
+                selectedDistrict={selectedDistrict}
+                selectedCategory={selectedCategory}
+                setSelectedDistrict={setSelectedDistrict}
+                setSelectedCategory={setSelectedCategory}
+            />
+            <YearSelector years={list_year} onYearSelect={handleYearSelect} />
+            <ProtestList data={data} />
+        </div>
+    );
+}

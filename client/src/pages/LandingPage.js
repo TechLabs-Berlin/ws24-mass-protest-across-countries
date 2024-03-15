4023873b1fc7b5f0260b4f28f2af8dc442ec88fd
nuclyd <nuclealydie@gmail.com>
2024-03-15 17:39:05 +0100

    update LandingPage.js

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,95 +1,39 @@
-// import SearchBar from "./SearchBar";
-export default function LandingPage() {
-  return (
-    <div>
-      <hr />
-      <main>
-        <div id="banner">
-          <br />
-          <br />
-          <br />
-          BANNER/Carrousel?
-          <br />
-          <br />
-          <br />
-        </div>
-        <hr />
-        <div>
-          {/* <SearchBar /> */}
-        </div>
-        <div id="search-buttons">
-          <div id="inner-navigation">
-            <label>
-              <select>
-                <option value="">Location</option>
-                <option value="">Charlottenburg-Wilmersdorf</option>
-                <option value="">Friedrichshain-Kreuzberg</option>
-                <option value="">Lichtenberg-Hohenschönhausen</option>
-                <option value="">Marzahn-Hellersdorf</option>
-                <option value="">Mitte</option>
-                <option value="">Neukölln</option>
-                <option value="">Pankow</option>
-                <option value="">Reinickendorf</option>
-                <option value="">Spandau</option>
-                <option value="">Steglitz-Zehlendorf</option>
-                <option value="">Tempelhof-Schöneberg</option>
-                <option value="">Treptow-Köpenick</option>
-              </select>
-            </label>
-            <label>
-              <select>
-                <option value="">Categories</option>
-                <option value="">Politics</option>
-                <option value="">Environment</option>
-                <option value="">Education</option>
-                <option value="">Human Rights</option>
-                <option value="">Women's Rights</option>
-                <option value="">(Finance/Economy)</option>
-              </select>
-            </label>
-          </div>
-          <hr />
-          <br />
-          <div id="protest-filters">
-            <form action="/protests" method="GET">
-              <button>Upcoming protests</button>
-            </form>
+import ProtestPage from "./ProtestPage";
 
-            <form action="/past-protests" method="GET">
-              <button>Past protests</button>
-            </form>
+export default function LandingPage() {
+    return (
+      <div>
+          <h1>Homepage</h1>
+        <main>
+          <hr></hr>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div id="banner">
+            BANNER/Carrousel?
           </div>
-          <hr />
-          <br />
-          <div id="protest-cards">
-            <div>
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <p>Protest info cards area</p>
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-            </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <hr></hr>
+          <div>
+          <br></br>
+          <form id="SearchBar">
+            <input type="text" id="search-bar" />
+            <button>Search</button>
+          </form>
+          <br></br> 
           </div>
-        </div>
-        <hr />
-        <footer>
-          <br />
-          <p>footer</p>
-          <br />
-          <p>
-            &copy; <i>FindMyProtest</i>
-          </p>
-        </footer>
-      </main>
-    </div>
-  );
-}
+          <div id="search-buttons">
+            <ProtestPage />
+          </div>
+          <hr></hr>
+          <footer>
+            <p><b>FOOTER</b></p>
+            <br></br>
+            <p>&copy; <i>FindMyProtest</i></p>
+          </footer>
+        </main>
+      </div>
+        )
+  }

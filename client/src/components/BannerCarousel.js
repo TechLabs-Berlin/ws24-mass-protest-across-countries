b3a022cc16b9f7b9b06872cecf5e6a54ce5326b7
annir3 <annika.reynders@googlemail.com>
2024-03-17 19:40:01 +0100

    added Carousel Card and Bar

---
+++ client/src/components/BannerCarousel.js
@@ -0,0 +1,45 @@
+import Carousel from "react-bootstrap/Carousel";
+
+function BannerCarousel() {
+  return (
+    <Carousel data-bs-theme="light">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1591259622709-bdb033b4be2b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvdGVzdHxlbnwwfDB8MHx8fDA%3D"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1592524138886-f4e3170d5ea7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cHJvdGVzdHxlbnwwfDB8MHx8fDA%3D"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1594230612502-606960bfe3c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb3Rlc3R8ZW58MHwwfDB8fHww"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default BannerCarousel;
---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleFormSubmit}>
+        <label>Enter Search Term</label>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;

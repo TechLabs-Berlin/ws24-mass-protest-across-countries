25b6a292eedaad4a745d41e176053625a9374b50
annir3 <annika.reynders@googlemail.com>
2024-03-28 19:22:48 +0100

    added Card Component to ProtestList.js

--- client/src/components/protest/ProtestList.js
+++ client/src/components/protest/ProtestList.js
@@ -1,22 +1,62 @@
-function ProtestList({ data }) {
-    const formatDate = (isoDate) => {
-        const date = new Date(isoDate);
-        return date.toLocaleDateString();
-    };
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import CardTitle from "react-bootstrap/esm/CardTitle";
+import Row from "react-bootstrap/esm/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import CardBody from "react-bootstrap/esm/CardBody";
+import Toast from "react-bootstrap/Toast";
 
-    return (
-        <div>
-            {data.map((item, index) => (
-                <div key={index}>
-                    <img src={item.imageUrl} alt="random protest image" /><br/>
-                    Date: {formatDate(item.event_date)}<br/>
-                    District: {item.district}<br/>
-                    Theme: {item.categories}<br/>
-                    Description: {item.description}<br/><hr/>
-                </div>
-            ))}
-        </div>
-    );
+function ProtestList({ data }) {
+  const formatDate = (isoDate) => {
+    const date = new Date(isoDate);
+    return date.toLocaleDateString();
+  };
+  const [show, setShow] = useState(true);
+  const toggleShow = () => setShow(!show);
+
+  return (
+    <div>
+      <Row xs={3} md={4} className="g-4">
+        {data.map((item, index) => (
+          <Col key={index}>
+            <Card>
+              <Card.Img
+                src="https://source.unsplash.com/random/?protests"
+                alt="random protest image"
+                height={200}
+              />
+              <CardBody>
+                <CardTitle> {formatDate(item.event_date)}</CardTitle>
+
+                <ListGroup variant="flush">
+                  <ListGroup.Item>Theme: {item.categories}</ListGroup.Item>
+                  <ListGroup.Item>District: {item.district}</ListGroup.Item>
+                </ListGroup>
+                <Button onClick={toggleShow} variant="danger">
+                  More Info
+                </Button>
+                <Toast onClose={toggleShow} show={show} animation={false}>
+                  <Toast.Header>
+                    <img
+                      src="holder.js/20x20?text=%20"
+                      className="rounded me-2"
+                      alt=""
+                    />
+                    <strong className="me-auto">
+                      What is this Protest about
+                    </strong>
+                  </Toast.Header>
+                  <Toast.Body>{item.description}</Toast.Body>
+                </Toast>
+              </CardBody>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
 
 export default ProtestList;

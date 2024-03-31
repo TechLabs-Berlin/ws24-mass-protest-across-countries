20dac6f237fe2f08fe4022db3f8cea936596a4fd
nuclyd <nuclealydie@gmail.com>
2024-03-31 18:56:29 +0200

    update SearchBar component logic in client

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const DEV_URL = "http://localhost:8000/";
 
 function SearchBar({ onSubmit }) {
   const [term, setTerm] = useState("");
 
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    onSubmit(term);
+    try {
+      const url_param = '?searchterm=' + term;
+      const response = await axios.get(DEV_URL + "api/protest/search" + url_param);
+      const searchData = response.data.slice(0, 10);
+      onSubmit(searchData); // Call the onSubmit callback with the search results
+      setTerm("")
+    } catch (err) {
+      console.log(err.message);
+    }
   };
 
   const handleChange = (event) => {
@@ -16,8 +26,10 @@ function SearchBar({ onSubmit }) {
   return (
     <div className="search-bar">
       <form onSubmit={handleFormSubmit}>
-        <label>Enter Search Term</label>
-        <input value={term} onChange={handleChange} />
+        <label>
+            <input value={term} onChange={handleChange} />
+            <button type="submit">Search</button>
+        </label>
       </form>
     </div>
   );

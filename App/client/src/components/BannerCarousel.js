cf03b0c26d564788610bde7219e926712e2b052b
annir3 <annika.reynders@googlemail.com>
2024-04-05 20:38:18 +0200

    add new Banner Picture and Logo

--- App/client/src/components/BannerCarousel.js
+++ App/client/src/components/BannerCarousel.js
@@ -5,37 +5,42 @@ function BannerCarousel() {
     <Carousel data-bs-theme="light">
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1591259622709-bdb033b4be2b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvdGVzdHxlbnwwfDB8MHx8fDA%3D"
+          className="d-block h-50 w-100"
+          src="https://images.unsplash.com/photo-1592524138886-f4e3170d5ea7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cHJvdGVzdHxlbnwwfDB8MHx8fDA%3D"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1592524138886-f4e3170d5ea7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cHJvdGVzdHxlbnwwfDB8MHx8fDA%3D"
+          className="content-align-center h-50 w-50"
+          src="fmplogo.png"
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h5>
+            <a
+              className="text-danger"
+              style={{ textDecoration: "none" }}
+              href="http://localhost:3000/about"
+            >
+              MEET FIND MY PROTEST
+            </a>
+          </h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1594230612502-606960bfe3c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb3Rlc3R8ZW58MHwwfDB8fHww"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src="banner3.jpg" alt="Third slide" />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h5>
+            <a
+              className="text-black"
+              style={{ textDecoration: "none" }}
+              href="http://localhost:3000/analytics"
+            >
+              PROTEST ANALYTICS
+            </a>
+          </h5>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>

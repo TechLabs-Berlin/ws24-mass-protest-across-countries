9658b0f695b80cb43912059f99600824cd924b49
annir3 <annika.reynders@googlemail.com>
2024-04-05 21:49:53 +0200

    add Columns for Search Bar and Dropdowns

--- App/client/src/pages/ProtestPage.js
+++ App/client/src/pages/ProtestPage.js
@@ -4,94 +4,114 @@ import Dropdowns from "../components/protest/Dropdowns";
 import ProtestList from "../components/protest/ProtestList";
 import YearSelector from "../components/protest/YearSelector";
 import PeriodSelector from "../components/protest/PeriodSelector";
-import SearchBar from '../components/SearchBar';
+import SearchBar from "../components/SearchBar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const DEV_URL = "http://localhost:8000/";
 const list_year = [2021, 2022, 2023, 2024];
 
 function ProtestPage() {
-    const [data, setData] = useState([]);
-    const [originalData, setOriginalData] = useState([]); // Store original data
-    const [selectedYear, setSelectedYear] = useState("");
-    const [selectedPeriod, setSelectedPeriod] = useState("");
-    const [selectedCategory, setSelectedCategory] = useState("");
-    const [selectedDistrict, setSelectedDistrict] = useState("");
-    const [searchActive, setSearchActive] = useState(false); // Track if search is active
+  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]); // Store original data
+  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedPeriod, setSelectedPeriod] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedDistrict, setSelectedDistrict] = useState("");
+  const [searchActive, setSearchActive] = useState(false); // Track if search is active
 
-    const getProtestList = async () => {
-        try {
-            const queryParams = [
-                ...(selectedYear ? [`year=${selectedYear}`] : []),
-                ...(selectedPeriod ? [`event_date=${selectedPeriod}`] : []),
-                ...(selectedCategory ? [`category=${selectedCategory}`] : []),
-                ...(selectedDistrict ? [`district=${selectedDistrict}`] : [])
-            ];
-            
-            const url_param = queryParams.length > 0 ? '?' + queryParams.join('&') : '';
+  const getProtestList = async () => {
+    try {
+      const queryParams = [
+        ...(selectedYear ? [`year=${selectedYear}`] : []),
+        ...(selectedPeriod ? [`event_date=${selectedPeriod}`] : []),
+        ...(selectedCategory ? [`category=${selectedCategory}`] : []),
+        ...(selectedDistrict ? [`district=${selectedDistrict}`] : []),
+      ];
 
-            const response = await axios.get(DEV_URL + "api/protest" + url_param);
-            const protestData = response.data.slice(0, 10);
-            setData(protestData);
-            setOriginalData(protestData); // Update original data when fetching
-            console.log(protestData);
-        } catch (err) {
-            console.log(err.message);
-        }
-    };
+      const url_param =
+        queryParams.length > 0 ? "?" + queryParams.join("&") : "";
 
-    useEffect(() => {
-        getProtestList();
-    }, [selectedYear, selectedPeriod, selectedCategory, selectedDistrict]); // Add selected values as dependencies to trigger the effect when they change
-    
-    
-    //state function to filter by year:
-    const handleYearSelect = async (year) => {
-        setSelectedYear(year); 
-    };
-    
-    //state function to trigger data search:
-    const handleSearchSubmit = async (searchData) => {
-        setData(searchData);
-        setSearchActive(true); // Set search as active
-    };
+      const response = await axios.get(DEV_URL + "api/protest" + url_param);
+      const protestData = response.data.slice(0, 10);
+      setData(protestData);
+      setOriginalData(protestData); // Update original data when fetching
+      console.log(protestData);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
-    //state function to reset filters:
-    const handleResetFilters = () => {
-        setSelectedYear("");
-        setSelectedPeriod("");
-        setSelectedCategory("");
-        setSelectedDistrict("");
-        setSearchActive(false); // Clear search and set as inactive
-        setData(originalData); // Restore original data when resetting filters
+  useEffect(() => {
+    getProtestList();
+  }, [selectedYear, selectedPeriod, selectedCategory, selectedDistrict]); // Add selected values as dependencies to trigger the effect when they change
 
-        // Revert YearSelector to default option
-        document.querySelector("#yearSelector").value = "";
-    };
+  //state function to filter by year:
+  const handleYearSelect = async (year) => {
+    setSelectedYear(year);
+  };
 
-    return (
-        <div>
-            <SearchBar onSubmit={handleSearchSubmit}/>
+  //state function to trigger data search:
+  const handleSearchSubmit = async (searchData) => {
+    setData(searchData);
+    setSearchActive(true); // Set search as active
+  };
+
+  //state function to reset filters:
+  const handleResetFilters = () => {
+    setSelectedYear("");
+    setSelectedPeriod("");
+    setSelectedCategory("");
+    setSelectedDistrict("");
+    setSearchActive(false); // Clear search and set as inactive
+    setData(originalData); // Restore original data when resetting filters
+
+    // Revert YearSelector to default option
+    document.querySelector("#yearSelector").value = "";
+  };
+
+  return (
+    <div>
+      <Container className="my-5">
+        <Row>
+          <Col xs={3}>
+            <SearchBar onSubmit={handleSearchSubmit} />
+          </Col>
+          <Col xs={3}>
             <Dropdowns
-                selectedDistrict={selectedDistrict}
-                selectedCategory={selectedCategory}
-                setSelectedDistrict={setSelectedDistrict}
-                setSelectedCategory={setSelectedCategory}
+              selectedDistrict={selectedDistrict}
+              selectedCategory={selectedCategory}
+              setSelectedDistrict={setSelectedDistrict}
+              setSelectedCategory={setSelectedCategory}
             />
-            <YearSelector 
-                years={list_year} 
-                selectedYear={selectedYear} 
-                onYearSelect={handleYearSelect}
-                />
-            <PeriodSelector 
-                selectedPeriod={selectedPeriod}
-                setSelectedPeriod={setSelectedPeriod}
+          </Col>
+          <Col>
+            <YearSelector
+              years={list_year}
+              selectedYear={selectedYear}
+              onYearSelect={handleYearSelect}
             />
-            <div> 
-                <button onClick={handleResetFilters}>Clear</button>
+          </Col>
+          <Col>
+            <PeriodSelector
+              selectedPeriod={selectedPeriod}
+              setSelectedPeriod={setSelectedPeriod}
+            />
+          </Col>
+          <Col>
+            <div>
+              <button onClick={handleResetFilters}>Clear</button>
             </div>
-            <ProtestList data={searchActive ? data : originalData} /> {/* Conditionally render based on searchActive state */}
-        </div>
-    );
+          </Col>
+          <Row>
+            <ProtestList data={searchActive ? data : originalData} />{" "}
+            {/* Conditionally render based on searchActive state */}
+          </Row>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default ProtestPage;

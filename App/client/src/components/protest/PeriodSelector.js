fe7deeb22e97f73bf2f44e93e111b8f229d02999
nuclyd <nuclealydie@gmail.com>
2024-04-08 21:12:18 +0200

    update pick dates and style in PeriodSelector

--- App/client/src/components/protest/PeriodSelector.js
+++ App/client/src/components/protest/PeriodSelector.js
@@ -1,18 +1,18 @@
-function PeriodSelector({ selectedPeriod, setSelectedPeriod }) {
-    const nextWeek = '2024-02-01T00:00:00.000Z';
-    const nextMonth = '2023-12-31T23:59:59.999Z';
+import { Form }  from 'react-bootstrap';
 
-    return (
-        <div>
-            <label>
-                <select value={selectedPeriod} onChange={(e) => setSelectedPeriod(e.target.value)}>
-                    <option value="">Upcoming protests</option>
-                    <option value={nextWeek}>Next week</option> 
-                    <option value={nextMonth}>Next month</option>             
-                </select>
-            </label>
-        </div>
-    );
+function PeriodSelector({ selectedPeriod, setSelectedPeriod }) {
+  const nextWeek = '2024-02-20T00:00:00.000Z';
+  const nextMonth = '2024-03-01T23:59:59.999Z';
+
+  return (
+    <Form.Select 
+      className="form-select-lg"
+      value={selectedPeriod} onChange={(e) => setSelectedPeriod(e.target.value)}>
+      <option value="">Upcoming protests</option>
+      <option value={nextWeek}>Next week</option> 
+      <option value={nextMonth}>Next month</option>             
+    </Form.Select>
+  );
 }
 
 export default PeriodSelector;

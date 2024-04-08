c862e7ae556d017d0a17ca87e137894a6992556d
nuclyd <nuclealydie@gmail.com>
2024-04-08 21:30:29 +0200

    correct import path in LandingPage

--- App/client/src/pages/LandingPage.js
+++ App/client/src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap/Container";
+import { Container } from "react-bootstrap";
 import ProtestPage from "./ProtestPage";
 import Banner from "../components/Banner";
 

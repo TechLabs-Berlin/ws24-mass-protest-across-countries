879a771d1a513b23ae6fbb9b14a3ba6fd07ca605
nuclyd <nuclealydie@gmail.com>
2024-04-08 21:09:10 +0200

    update Banner style

--- App/client/src/components/Banner.js
+++ App/client/src/components/Banner.js
@@ -1,27 +1,45 @@
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Image from "react-bootstrap/Image";
+import '../index.css';
+import { Container, Carousel, Col, Row, Image, Button } from "react-bootstrap";;
 
 function Banner() {
   return (
-    <Container>
-      <Row className="justify-content-center align-items-center h-100">
-        <Col xs={6} md={4}>
-          <Image src="fmplogo.png" alt="logo" fluid />
-        </Col>
-        <Col lg="10" xl="8">
-          <figure className="mb-0 text-center">
-            <p>
-              <span className="text-danger fs-4">
-                Meet Find My Protest, a website designed for Berlin residents
-                who care about what's happening in their city and want to get
-                involved in protests.
-              </span>
-            </p>
-          </figure>
-        </Col>
-      </Row>
+    <Container fluid className="position-relative rounded">
+      {/* Logo */}
+      <Col xs={6} md={4} lg={3}>
+        <Image src="fmplogo.png" alt="logo" className="logo" fluid />
+      </Col>
+      {/* Carousel */}
+      <Carousel className="carousel rounded">
+        <Carousel.Item interval={3000}>
+        <div
+            className="d-block w-100 carousel-image"
+            style={{ backgroundImage: "url('banner6.jpg')" }}
+          ></div>
+        <Carousel.Caption>
+             <div className="text-white bg-dark border rounded py-2 fs-2">
+              Meet Find My Protest, a website designed for Berlin residents...
+             </div>
+        </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <div
+              className="d-block w-100 carousel-image"
+              style={{ backgroundImage: "url('banner7.jpg')" }}
+            ></div>
+          <Carousel.Caption>
+             <div className="text-white bg-dark border rounded py-2 mx-0 fs-2">
+               ...who care about what's happening in their city and want to get
+               involved.
+             </div>
+          </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <div
+              className="d-block w-100 carousel-image"
+              style={{ backgroundImage: "url('banner1.jpg')" }}
+            ></div>
+          </Carousel.Item>
+      </Carousel>
     </Container>
   );
 }

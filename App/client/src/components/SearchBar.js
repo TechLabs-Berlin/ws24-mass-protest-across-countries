74bca9d542d800f3384e2faf4d16ed57c808131e
nuclyd <nuclealydie@gmail.com>
2024-04-08 21:03:16 +0200

    refactor SearchBar logic + update style

--- App/client/src/components/SearchBar.js
+++ App/client/src/components/SearchBar.js
@@ -1,40 +1,49 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// In the frontend component
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form, InputGroup }  from 'react-bootstrap';
 
 const DEV_URL = "http://localhost:8000/";
 
 function SearchBar({ onSubmit }) {
-  const [term, setTerm] = useState("");
+    const [searchterm, setSearchterm] = useState("");
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const url_param = "?searchterm=" + term;
-      const response = await axios.get(
-        DEV_URL + "api/protest/search" + url_param
-      );
-      const searchData = response.data.slice(0, 10);
-      onSubmit(searchData); // Call the onSubmit callback with the search results
-      setTerm("");
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const url_param = '?searchterm=' + searchterm;
+            console.log('Sending search request with term:', searchterm);
+            const response = await axios.get(DEV_URL + "api/search" + url_param);
+            console.log('Response received:', response.data);
+            const searchData = response.data;
+            onSubmit(searchData); // Call the onSubmit callback with the search results
+            setSearchterm("")
+        } catch (err) {
+            console.log('Error occurred:', err.message);
+        }
+    };
 
-  const handleChange = (event) => {
-    setTerm(event.target.value);
-  };
+    const handleChange = (event) => {
+        setSearchterm(event.target.value);
+    };
 
-  return (
-    <div className="search-bar">
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          <input placeholder="Search" value={term} onChange={handleChange} />
-        </label>
-      </form>
-    </div>
-  );
+    return (
+        <Form onSubmit={handleFormSubmit}>
+            <Form.Group lg="auto" controlId="validationCustomUsername">
+                <InputGroup hasValidation>
+                <Form.Control
+                    className="form-control-lg"
+                    type="text"
+                    value={searchterm}
+                    onChange={handleChange}
+                    placeholder="Search Protest"
+                    aria-describedby="inputGroupPrepend"
+                    required
+                />
+                </InputGroup>
+            </Form.Group>
+        </Form>
+    );
 }
 
 export default SearchBar;

53b0f098f5e86157865a3933e56c7b74036a81f7
nuclyd <nuclealydie@gmail.com>
2024-04-08 21:14:15 +0200

    update style in CategorySelector

--- App/client/src/components/protest/CategorySelector.js
+++ App/client/src/components/protest/CategorySelector.js
@@ -1,21 +1,19 @@
+import { Form } from "react-bootstrap";
+
 function CategorySelector({ selectedCategory, setSelectedCategory }) {
   return (
-    <div>
-      <label>
-        <select
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-        >
-          <option value="">Category</option>
-          <option value="Local">Local</option>
-          <option value="War / Peace">War/Peace</option>
-          <option value="Environment">Environment</option>
-          <option value="Health">Health</option>
-          <option value="Human Rights">Human Rights</option>
-          <option value="Economy">Economy</option>
-        </select>
-      </label>
-    </div>
+    <Form.Select className="form-select-lg"
+      value={selectedCategory}
+      onChange={(e) => setSelectedCategory(e.target.value)}
+    >
+      <option value="">Select category</option>
+      <option value="Local">Local</option>
+      <option value="War / Peace">War/Peace</option>
+      <option value="Environment">Environment</option>
+      <option value="Health">Health</option>
+      <option value="Human Rights">Human Rights</option>
+      <option value="Economy">Economy</option>
+    </Form.Select>
   );
 }
 

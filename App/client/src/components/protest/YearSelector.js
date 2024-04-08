25499d1f451c2937cefbb1245e774fc4ad3c05c7
nuclyd <nuclealydie@gmail.com>
2024-04-08 21:11:08 +0200

    update style in YearSelector

--- App/client/src/components/protest/YearSelector.js
+++ App/client/src/components/protest/YearSelector.js
@@ -1,16 +1,16 @@
-function YearSelector({ years, onYearSelect }) {
-    return (
-        <div>
-            <label>
-                <select id="yearSelector" onChange={(e) => onYearSelect(e.target.value)}>
-                    <option value="">Past protests</option>
-                    {years.map(year => (
-                        <option key={year} value={year}>{year}</option>
-                    ))}         
-                </select>
-            </label>
-        </div>
-    );
+import { Form }  from 'react-bootstrap';
+
+function YearSelector({ years, selectedYear, onYearSelect }) {
+  return (
+    <Form.Select
+      className="form-select-lg"
+      value={selectedYear} onChange={(e) => onYearSelect(e.target.value)}>
+      <option value="">Past protests</option>
+      {years.map(year => (
+          <option key={year} value={year}>{year}</option>
+      ))}                     
+    </Form.Select>
+  );
 }
 
 export default YearSelector;

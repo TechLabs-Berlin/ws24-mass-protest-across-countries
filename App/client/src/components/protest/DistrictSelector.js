210349d81b1c66951c6fa05ce97ea3e320e948a0
nuclyd <nuclealydie@gmail.com>
2024-04-08 21:13:28 +0200

    update style in DistrictSelector

--- App/client/src/components/protest/DistrictSelector.js
+++ App/client/src/components/protest/DistrictSelector.js
@@ -1,32 +1,31 @@
+import { Form }  from 'react-bootstrap';
+
 function DistrictSelector({ selectedDistrict, setSelectedDistrict }) {
   return (
-    <div>
-      <label>
-        <select
-          value={selectedDistrict}
-          onChange={(e) => setSelectedDistrict(e.target.value)}
-        >
-          <option value="">Location</option>
-          <option value="Charlottenburg-Wilmersdorf">
-            Charlottenburg-Wilmersdorf
-          </option>
-          <option value="Friedrichshain-Kreuzberg">
-            Friedrichshain-Kreuzberg
-          </option>
-          <option value="Lichtenberg">Lichtenberg</option>
-          <option value="Marzahn-Hellersdorf">Marzahn-Hellersdorf</option>
-          <option value="Mitte">Mitte</option>
-          <option value="Neukolln">Neukölln</option>
-          <option value="Pankow">Pankow</option>
-          <option value="Reinickendorf">Reinickendorf</option>
-          <option value="Spandau">Spandau</option>
-          <option value="Steglitz-Zehlendorf">Steglitz-Zehlendorf</option>
-          <option value="Tempelhof-Schoneberg">Tempelhof-Schöneberg</option>
-          <option value="Treptow-Kopenick">Treptow-Köpenick</option>
-        </select>
-      </label>
-    </div>
+    <Form.Select
+      className="form-select-lg"
+      value={selectedDistrict}
+      onChange={(e) => setSelectedDistrict(e.target.value)}
+    >
+      <option value="">Select location</option>
+      <option value="Charlottenburg-Wilmersdorf">
+        Charlottenburg-Wilmersdorf
+      </option>
+      <option value="Friedrichshain-Kreuzberg">
+        Friedrichshain-Kreuzberg
+      </option>
+      <option value="Lichtenberg">Lichtenberg</option>
+      <option value="Marzahn-Hellersdorf">Marzahn-Hellersdorf</option>
+      <option value="Mitte">Mitte</option>
+      <option value="Neukolln">Neukölln</option>
+      <option value="Pankow">Pankow</option>
+      <option value="Reinickendorf">Reinickendorf</option>
+      <option value="Spandau">Spandau</option>
+      <option value="Steglitz-Zehlendorf">Steglitz-Zehlendorf</option>
+      <option value="Tempelhof-Schoneberg">Tempelhof-Schöneberg</option>
+      <option value="Treptow-Kopenick">Treptow-Köpenick</option>
+    </Form.Select>
   );
 }
-
-export default DistrictSelector;
+  
+  export default DistrictSelector;
